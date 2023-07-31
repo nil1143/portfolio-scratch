dcddb88efc88332b52c19d95f9d708d3093b6500
nil1143 <tnil1143@gmail.com>
2023-07-31 20:49:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+//  NAV
+
+const hamburger = document.querySelector('.hamburger')
+const nav = document.querySelector('#nav')
+
+hamburger.addEventListener('click', () => {
+   
+    if(nav.classList.contains('hidden')) {
+        nav.classList.remove('hidden')
+    } else {
+        nav.classList.add('hidden')
+    }})
+
+
+const navLink= document.querySelectorAll('.nav-link')
+
+navLink.forEach(link => link.addEventListener('click', () => {
+    nav.classList.add('hidden')
+    xicon.classList.remove('fa-xmark')
+}
+))
+
+const xicon = document.querySelector('#icon')
+
+const changeIcon = () => {
+    xicon.classList.toggle('fa-xmark')
+}
+
+// CONTACT ME
+
+const sendBtn = document.querySelector('#send-btn')
+const modal = document.querySelector('#modal')
+
+sendBtn.addEventListener('click', e => 
+{
+    e.preventDefault();
+    checkInputs()
+    checkEmail(email)
+
+    checking()
+
+})
+
+const checking = () => {
+    let errorCount = 0
+
+    allInputs.forEach(el => {
+        if(el.classList.contains('inputs-error')) {
+            errorCount++;
+        }
+    })
+
+    if(errorCount === 0 ) {
+        modal.classList.remove('hidden')
+    }
+    console.log(errorCount);
+}
+
+const allInputs = document.querySelectorAll('.inputs')
+
+const checkInputs = () => {
+    allInputs.forEach(el => {
+        if(el.value == '') {
+      
+            el.classList.add('inputs-error');
+        } else {
+    
+            el.classList.remove('inputs-error')
+        }
+    })
+}
+
+const email = document.querySelector('#email')
+const message = document.querySelector('#message')
+const formbox = document.querySelector('#formbox')
+
+const checkEmail = email => {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if(email.value.match(validRegex)) {
+     
+        email.classList.remove('inputs-error')
+
+    } else { 
+
+        email.placeholder = 'Wrong email address!!'  
+        email.value = ''
+        email.classList.add('inputs-error')
+    }
+}
+
+const closeBtn = document.querySelector('#close-btn')
+
+closeBtn.addEventListener('click', e => {
+    e.preventDefault();
+    modal.classList.add('hidden')
+})
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        first: '#61892F', // darkgreen
+        second: '#86C232', // green
+        third: '#222629', // dark
+        fourth: '#474B4F', // gray
+        fifth: '#6B6E70' // silver
+      },
+      fontFamily: {
+        inconsolata: ['"Inconsolata"', 'monospace'],
+        rajdhani: ['"Rajdhani"', 'sans-serif'],
+        ysabeau1: ['"Ysabeau Office"', 'sans-serif'],
+        ysabeau2: ['"Ysabeau SC"', 'sans-serif']
+      },
+      boxShadow: {
+        neon: "0 0 5px theme('colors.first')"
+      }
+    },
+  },
+  plugins: [],
+}
+
